3c3c6031bfc5766ee06d6a73ef72e83f97039471
foldu <foldu@protonmail.com>
2023-02-27 00:27:39 +0100

    nushell: 0.75.0 -> 0.76.0
    Diff: https://github.com/nushell/nushell/compare/0.75.0...0.76.0

--- packages/nushell/default.nix
+++ packages/nushell/default.nix
@@ -21,16 +21,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "nushell";
-  version = "0.75.0";
+  version = "0.76.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = version;
-    sha256 = "sha256-u8/SvuR/RpJaBX4Dr3Onrk0AVpIAeVb+399+NUpgkfI=";
+    sha256 = "sha256-dGsnbKsg0nQFFXZDRDei2uGhGWEQSeSHGpXJp+8QUC8=";
   };
 
-  cargoSha256 = "sha256-hnSumfZd9ylEx3dkTGW2s4VSv107MHOn21ytOcimhPw=";
+  cargoSha256 = "";
 
   # enable pkg-config feature of zstd
   cargoPatches = [ ./zstd-pkg-config.patch ];
