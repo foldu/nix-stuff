a245093a38af96cac6fc5344bc6b5e767fd07791
foldu <foldu@protonmail.com>
2023-08-01 16:39:09 +0200

    Fix writeNu* for versions > 0.83

--- packages/nu-script.nix
+++ packages/nu-script.nix
@@ -9,10 +9,15 @@ let
     let
       content = if file != null then builtins.readFile file else source;
       binPath = lib.strings.makeBinPath path;
+      pathDecl =
+        if pkgs.lib.versionAtLeast pkgs.nushell.version "0.83.0" then
+          "$env.PATH"
+        else
+          "let-env PATH";
     in
     fun name ''
       #!${pkgs.nushell}/bin/nu
-      let-env PATH = ($env.PATH | append ('${binPath}' | split row ":"))
+      ${pathDecl} = ($env.PATH | append ('${binPath}' | split row ":"))
       ${content}
     '';
 in
